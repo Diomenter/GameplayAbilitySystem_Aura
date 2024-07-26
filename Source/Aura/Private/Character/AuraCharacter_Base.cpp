817b45d3b7d735be4a461150418499ebaae1b0e6
Diomenter <ghostofwarriorz@gmail.com>
2024-07-26 20:37:26 +0300

    Player and enemy characters

---
+++ Source/Aura/Private/Character/AuraCharacter_Base.cpp
@@ -0,0 +1,23 @@
+
+
+
+#include "Character/AuraCharacter_Base.h"
+
+
+AAuraCharacter_Base::AAuraCharacter_Base()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacter_Base::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+
+
+
---
+++ Source/Aura/Public/Character/AuraCharacter_Base.h
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacter_Base.generated.h"
+
+UCLASS(Abstract)
+class AURA_API AAuraCharacter_Base : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	AAuraCharacter_Base();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+	
+
+};
